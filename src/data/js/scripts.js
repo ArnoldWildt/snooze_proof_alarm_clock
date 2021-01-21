abd8bc8afd90fe4e796e99d0375cba6ad4d9c57b
Arnold Wildt <arnoldwildt@gmail.com>
2021-01-21 22:01:06 +0100

    updated website (set alarm)

--- src/data/js/scripts.js
+++ src/data/js/scripts.js
@@ -1,13 +1,18 @@
-function openNav() {
+function open_nav() {
   document.getElementsByClassName("container")[0].classList.add("change");
   document.getElementById("mySidenav").style.width = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
+function close_nav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementsByClassName("container")[0].classList.remove("change");
 }
 
+function change_page() {}
+
 // Time , Is Activ, Icons
-function add_alarm() {}
+function add_alarm() {
+  document.getElementsByClassName("float")[0].classList.toggle("disable");
+  //Change to Alarm setup page
+}
